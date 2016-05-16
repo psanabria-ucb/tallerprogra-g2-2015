3a4603c568990f030f2fd75249485a5c910e4f32
Dani Illanes <dany.gaara.ds@gmail.com>
2016-05-16 03:35:59 -0400

    probando Clientes

---
+++ src/main/java/bo/edu/ucbcba/videoclub/model/Client.java
@@ -0,0 +1,7 @@
+package bo.edu.ucbcba.videoclub.model;
+
+/**
+ * Created by privado on 16/05/2016.
+ */
+public class Client {
+}
---
+++ src/main/java/bo/edu/ucbcba/videoclub/view/ClientRegister.java
@@ -0,0 +1,7 @@
+package bo.edu.ucbcba.videoclub.view;
+
+/**
+ * Created by privado on 16/05/2016.
+ */
+public class ClientRegister {
+}
